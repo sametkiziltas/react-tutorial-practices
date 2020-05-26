2d706d1ee00f17faef94b89de809a24ab759ede1
Samet Kızıltaş <sametkiziltas@outlook.com.tr>
2020-05-26 12:27:34 +0300

    Composition vs Inheritance - Containment

--- my-app1/src/App.js
+++ my-app1/src/App.js
@@ -1,110 +1,52 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import Form from "react-bootstrap/Form";
 
-const scaleNames = {
-  c: "Santigrat",
-  f: "Fahrenayt",
-};
-
-function BoilingVerdict(props) {
-  if (props.celsius >= 100) {
-    return <p>Su Kaynıyor.</p>;
-  }
-  return <p>Su Kaynamaz.</p>;
+function FancyBorder(props) {
+  return (
+    <div className={"FancyBorder FancyBorder-" + props.color}>
+      {props.children}
+    </div>
+  );
 }
 
-function toCelsius(fahrenheit) {
-  return ((fahrenheit - 32) * 5) / 9;
+function WelcomeDialog(props) {
+  return (
+    <FancyBorder color="red">
+      <h1 className="Dialog-title">Welcome {props.name}</h1>
+      <p className="Dialog-message">Thank you for visiting our App !</p>
+    </FancyBorder>
+  );
 }
 
-function toFahrenheit(celsius) {
-  return (celsius * 9) / 5 + 32;
+function Contacts() {
+  return <div className="Contacts"></div>;
 }
 
-function tryConvert(temperature, convert) {
-  const input = parseFloat(temperature);
-  if (Number.isNaN(input)) {
-    return "";
-  }
-  const output = convert(input);
-  const rounded = Math.round(output * 1000) / 1000;
-  return rounded.toString();
+function Chat() {
+  return <div className="Chat"></div>;
 }
 
+function SplitPane(props) {
+  return (
+    <div className="SplitPane">
+      <div className="SplitPane-left">{props.left}</div>
+      <div className="SplitPane-right">{props.right}</div>
+    </div>
+  );
+}
 
-
-class TemperatureInput extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(e) {
-    this.props.onTemperatureChange(e.target.value);
-  }
-
+class App extends React.Component {
   render() {
-    const temperature = this.props.temperature;
-    const scale = this.props.scale;
-
-    return (
-      <fieldset>
-        <legend>{scaleNames[scale]} cinsinden sıcaklık girin:</legend>
-        <input value={temperature} onChange={this.handleChange} />
-      </fieldset>
-    );
-  }
-}
-
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
-    this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
-    this.state = { temperature: '', scale: 'c' };
-  }
-  handleCelsiusChange(temperature) {
-    this.setState({ scale: 'c', temperature });
-  }
-
-  handleFahrenheitChange(temperature) {
-    this.setState({ scale: 'f', temperature });
-  }
-
-  render() {
-    const scale = this.state.scale;
-    const temperature = this.state.temperature;
-
-    const celsius =
-      scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
-      
-    const fahrenheit =
-      scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
-
+    const name = "Samet";
     return (
       <div className="App">
         <header className="App-header">
-          <TemperatureInput
-            scale="c"
-            temperature={celsius}
-            onTemperatureChange={this.handleCelsiusChange}
-          ></TemperatureInput>
-
-          <TemperatureInput
-            scale="f"
-            temperature={fahrenheit}
-            onTemperatureChange={this.handleFahrenheitChange}
-          ></TemperatureInput>
-
-          <BoilingVerdict celsius={parseFloat(celsius)} />
+          <WelcomeDialog name={name} />
+          <SplitPane left={<Contacts />} right={<Chat />} />
         </header>
       </div>
     );
   }
 }
 
-export default Calculator;
+export default App;
