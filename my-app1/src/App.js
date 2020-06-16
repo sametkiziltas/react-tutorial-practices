fa16c4581b6cd635d272b483a80b45b462ebf5e8
Samet Kızıltaş <sametkiziltas@outlook.com.tr>
2020-06-16 12:16:09 +0300

    Introducing Hooks

--- my-app1/src/App.js
+++ my-app1/src/App.js
@@ -1,73 +1,26 @@
-import React from "react";
+import React, { useState ,useEffect} from "react";
 import "./App.css";
 
-function FancyBorder(props) {
+function Example() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  })
   return (
-    <div className={"FancyBorder FancyBorder-" + props.color}>
-      {props.children}
+    <div>
+      <p>you clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me!</button>
     </div>
   );
 }
 
-function Dialog(props) {
-  return (
-    <FancyBorder color="blue">
-      <h1 className="Dialog-title">{props.title}</h1>
-      <p className="Dialog-message">{props.message}</p>
-      {props.children}
-    </FancyBorder>
-  );
-}
-
-function WelcomeDialog(props) {
-  return (
-    <Dialog
-      title={"Hoşgeldin " + props.name}
-      message="Uzay aracımızı ziyaret ettiğiniz için teşekkür ederiz!"
-    />
-  );
-}
-
-class SignUpDialog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSignUp = this.handleSignUp.bind(this);
-    this.state = { login: "" };
-  }
-
-  render() {
-    return (
-      <Dialog title="Mars Keşif Programı" message="Size nasıl başvurabiliriz?">
-        <input 
-          value={this.state.login}
-          onChange={this.handleChange} 
-        />
-        <button 
-          onClick={this.handleSignUp}
-        >Beni kaydet!</button>
-      </Dialog>
-    );
-  }
-
-  handleChange(e) {
-    this.setState({ login: e.target.value });
-  }
-
-  handleSignUp() {
-    alert(`Welcome aboard, ${this.state.login}!`);
-  }
-}
 class App extends React.Component {
   render() {
-    const name = "Samet";
     return (
       <div className="App">
         <header className="App-header">
-
-          <WelcomeDialog name={name} />
-          <SignUpDialog></SignUpDialog>
-          
+          <Example></Example>
         </header>
       </div>
     );
